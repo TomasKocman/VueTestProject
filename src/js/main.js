0fbcb8f0e028b4275dd33dbceed20ba7ab193b3b
kocmant <t.kocmancz@gmail.com>
2017-08-02 15:59:36 +0200

    Transitions

--- src/js/main.js
+++ src/js/main.js
@@ -3,9 +3,28 @@ import Vue from 'vue'
 new Vue({
     el: '#app',
     data: {
+        syllabus: [ 'HTML', 'CSS', 'Scratch', 'JavaScript', 'Python' ],
+
+        buses: [ 1, 2, 3, 4, 5],
+        nextBus: 6,
     },
     methods: {
+        completed(topic) {
+            let index = this.syllabus.indexOf(topic);
+            this.syllabus.splice(index, 1);
+        },
     },
     computed: {
+    },
+    mounted() {
+        setInterval(() => {
+            const headOrTail = () => Math.random() > 0.5;
+            if (headOrTail()) {
+                this.buses.push(this.nextBus);
+                this.nextBus += 1;
+            }
+            else
+                this.buses.splice(0, 1);
+        }, 2000)
     }
 });
