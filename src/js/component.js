33faf096de4f7bd6b5d957feeb6a44c7074a0924
kocmant <t.kocmancz@gmail.com>
2017-08-28 14:34:23 +0200

    Added components.

---
+++ src/js/component.js
@@ -0,0 +1,10 @@
+
+var lightBulb = {
+	template: `
+        <div class="light-bulb">
+            <p>💡 light bulb</p>
+        </div>
+    `
+};
+
+export { lightBulb };
--- src/js/main.js
+++ src/js/main.js
@@ -1,87 +1,77 @@
 import Vue from 'vue'
+const component = require('./component');
 
 // App1
 new Vue({
-    el: '#app',
-    data: {
-        syllabus: [ 'HTML', 'CSS', 'Scratch', 'JavaScript', 'Python' ],
-
-        buses: [ 1, 2, 3, 4, 5],
-        nextBus: 6,
-    },
-    methods: {
-        completed(topic) {
-            let index = this.syllabus.indexOf(topic);
-            this.syllabus.splice(index, 1);
-        },
-    },
-    mounted() {
-        setInterval(() => {
-            const headOrTail = () => Math.random() > 0.5;
-            if (headOrTail()) {
-                this.buses.push(this.nextBus);
-                this.nextBus += 1;
-            }
-            else
-                this.buses.splice(0, 1);
-        }, 2000)
+	el: '#app',
+    components: {
+	    'light-bulb': component.lightBulb
     }
 });
 
 // App2
-new Vue({
-    el: '#app2',
-    data: {
-        height: 0
-    },
-    methods: {
-        move (event) {
-            const newHeight = Number(event.target.value);
-            const _this = this;
-            const animate = (time) => {
-                requestAnimationFrame(animate);
-                TWEEN.update(time)
-            };
-            new TWEEN.Tween({ H: this.height })
-                .easing(TWEEN.Easing.Bounce.Out)
-                .to({ H: newHeight }, 1000)
-                .onUpdate(function () {
-                    _this.height = this.H
-                })
-                .start();
-            animate();
-        }
-    }
+Vue.component('sound-icon', {
+	template: "<span>{{ soundEmojis[level] }}</span>",
+	props: {
+		level: [Number]
+	},
+	data() {
+		return {
+			soundEmojis: ['🔊', '🔔', '📳', '🚨' ]
+		}
+	}
+});
 
+new Vue({
+	el: '#app2',
+	data: {
+		soundLevel: 0
+	}
 });
 
 // App3
+let line = 0;
+
+var bus = new Vue();
+
+Vue.component('blabber', {
+	template: "<p>{{ dialogue[currentLine] }}</p>",
+	props: {
+		iceBreaker: {
+			type: Boolean,
+			default: false
+		}
+	},
+	data() {
+		return {
+			currentLine: this.iceBreaker ? 0 : -1,
+			dialogue: [
+				'hello',
+				'how are you?',
+				'fine thanks',
+				"let's go drink!",
+				'alright, where?',
+				"to hello's bar",
+				'hello?'
+			]
+		}
+	},
+	mounted() {
+		if (this.iceBreaker)
+			bus.$emit('line', 0);
+	},
+	created() {
+		bus.$on('line', line => {
+			// Is not the line I just sent.
+			if (line !== this.currentLine)
+				setTimeout(() => {
+					this.currentLine = (line + 1) % this.dialogue.length;
+					bus.$emit('line', this.currentLine)
+				}, 2000);
+		})
+	},
+});
+
 new Vue({
-    el: '#app3',
-    data: {
-        kisses: 0,
-        kindOfTransformation: 'fade',
-        transformationMode: 'in-out'
-    },
-    computed: {
-        transformation() {
-            if (this.kisses < 3)
-                return 'frog';
-            else if (this.kisses >= 3 && this.kisses <= 5) {
-                this.transformationMode = 'out-in';
-                return 'princess';
-            }
-            else if (this.kisses > 5) {
-                this.kindOfTransformation = 'zoom';
-                return 'santa'
-            }
-        }
-    },
-    emoji() {
-        switch (this.transformation) {
-            case 'frog':     return '🐸';
-            case 'princess': return '👸';
-            case 'santa':    return '🎅';
-        }
-    }
+	el: '#app3',
 });
